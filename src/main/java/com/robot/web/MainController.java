127f400a67a5688d83a48b3dc03dd3ccfd8660d9
吴洋 <yang.wu06@ele.me>
2018-08-16 23:23:33 +0800

    update

--- src/main/java/com/robot/web/MainController.java
+++ src/main/java/com/robot/web/MainController.java
@@ -1,5 +1,9 @@
 package com.robot.web;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,12 +22,52 @@ import lombok.Data;
 public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
     @ResponseBody
-    String home(@RequestBody String request) {
+    String home(@RequestBody Request request) throws Exception {
+        String content = request.getText().getContent();
         Body body = new Body();
         body.setMsgtype("text");
-        body.setText(Text.builder().content(request).build());
-        body.setAt(At.builder().atMobiles(Arrays.asList("18856710282")).build());
+        body.setText(Text.builder().content(tuling(content)).build());
+        body.setAt(At.builder().atDingtalkIds(Arrays.asList(request.getSenderId())).build());
         return JSON.toJSONString(body);
+    }
+
+    public String tuling(String context) throws Exception {
+        String APIKEY = "402536689fcf4282ae1f213e70c6a819";
+        String getURL = "http://www.tuling123.com/openapi/api?key=" + APIKEY + "&info=" + context;
+        URL getUrl = new URL(getURL);
+        HttpURLConnection connection = (HttpURLConnection) getUrl.openConnection();
+        connection.connect();
+        StringBuffer sb = null;
+
+        // 取得输入流，并使用Reader读取
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+            sb = new StringBuffer();
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } finally {
+            // 断开连接
+            connection.disconnect();
+        }
+        return sb == null ? "啥都没返回！" : JSON.parseObject(new String(sb), TuLingBody.class).getText();
+    }
+
+    @Data
+    public static class Request {
+        private String msgtype;
+        private Text text;
+        private Long createAt;
+        private String conversationType;
+        private String conversationId;
+        private String conversationTitle;
+        private String senderId;
+        private String senderNick;
+        private String senderStaffId;
+        private boolean isAdmin;
+        private String context;
+        private String chatbotCorpId;
+        private String chatbotUserId;
     }
 
     @Data
@@ -68,10 +112,17 @@ public class MainController {
         private String title;
         private String actionURL;
     }
+
     @Data
     @Builder
     public static class At {
         private List<String> atMobiles;
         private List<String> atDingtalkIds;
+    }
+    
+    @Data
+    public static class TuLingBody {
+        private Long code;
+        private String text;
     }
 }
