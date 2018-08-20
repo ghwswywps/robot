d26523a30f882830e16afc34ed8ce2099f83dab6
吴洋 <yang.wu06@ele.me>
2018-08-20 19:59:37 +0800

    update

---
+++ src/main/java/com/robot/web/WebConfiguration.java
@@ -0,0 +1,48 @@
+package com.robot.web;
+
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class WebConfiguration {
+
+    private static final String mapping = "/console/*";
+
+    @Bean
+    public ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings(mapping);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean shallowEtagHeaderFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new ShallowEtagHeaderFilter());
+        registration.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
+        registration.addUrlPatterns(mapping);
+        return registration;
+    }
+
+    @Bean
+    public ServletContextInitializer initializer() {
+        return new ServletContextInitializer() {
+
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.setInitParameter("p-name", "-value");
+            }
+        };
+    }
+}
