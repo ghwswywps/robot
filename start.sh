88a188d555fabd4f4a2da846cbe5e3dc4fc79cba
吴洋 <yang.wu06@ele.me>
2018-08-23 14:03:42 +0800

    update

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 kill -9 $(lsof -i tcp:8080 -t)
 git pull
 mvn clean package
-nohup java -jar target/testSpringBoot-0.0.1-SNAPSHOT.jar &
+nohup java -jar target/testSpringBoot-0.0.1-SNAPSHOT.jar -Xms512m -Xmx512m &
 tailf nohup.out
