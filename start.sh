a152da45ec49c441e09cf51a35ccc452397bf67e
吴洋 <yang.wu@zhangmen.com>
2019-06-21 00:05:12 +0800

    update

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 kill -9 $(lsof -i tcp:10924 -t)
 git pull
 mvn clean package
-nohup java -jar target/testSpringBoot-0.0.1-SNAPSHOT.jar -Xms512m -Xmx512m &
+nohup java -jar target/dingcb.jar -Xms512m -Xmx512m &
 tail -f nohup.out
